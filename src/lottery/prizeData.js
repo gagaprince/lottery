4fda44a9edb74906c2e27b6363656da3a1325bc3
wangzidong <wangzidong@meituan.com>
2023-01-18 12:02:33 +0800

    feat: 家庭抽奖

---
+++ src/lottery/prizeData.js
@@ -0,0 +1,120 @@
+export const prizeData = {
+    cfgData:{
+        COMPANY:"老宋家",
+        EACH_COUNT:[1,1,1,1,1,1,1,1,3,1,2],
+        prizes:[
+            {
+              type: 1,
+              count: 1,
+              text: "一等奖",
+              title: "红米平板10.6英寸2K屏",
+              img: "https://img12.360buyimg.com/n1/s450x450_jfs/t1/82846/1/22526/46578/635a47faE46c04846/43fa61d99b8a7cc8.jpg"
+            },
+            {
+              type: 2,
+              count: 1,
+              text: "一等奖",
+              title: "小米扫拖一体机器人",
+              img: "https://img10.360buyimg.com/n1/jfs/t1/223717/34/18675/35673/63c0f312F342c329e/3c5c135f97cc02b1.jpg"
+            },
+            {
+                type: 10,
+                count: 1,
+                text: "特别奖",
+                title: "叶罗丽娃娃",
+                img: "https://img.alicdn.com/bao/uploaded/TB1e4sFcAL0gK0jSZFtSutQCXXa.jpg"
+            },
+            {
+                type: 11,
+                count: 1,
+                text: "特别奖",
+                title: "八音盒旋转木马",
+                img: "https://img.alicdn.com/bao/uploaded/O1CN01q03qTD1zVLtzt1UsU_!!6000000006719-0-yinhe.jpg"
+            },
+            {
+              type: 3,
+              count: 1,
+              text: "二等奖",
+              title: "天猫精灵智慧屏",
+              img: "https://img11.360buyimg.com/n1/jfs/t1/23331/3/20278/63238/63ae784bF8019df68/b88fbeadc87683db.jpg"
+            },
+            {
+              type: 4,
+              count: 1,
+              text: "二等奖",
+              title: "小米智能家庭屏",
+              img: "https://img10.360buyimg.com/n1/s450x450_jfs/t1/55813/32/21320/390133/62da4cd0Ed4f73b06/ecc027b30fd709ee.jpg"
+            },
+            {
+              type: 5,
+              count: 1,
+              text: "二等奖",
+              title: "苏泊尔吸尘器",
+              img: "https://img12.360buyimg.com/n1/jfs/t1/17738/40/17564/368086/62c6ba8dE037a753b/69ce4c63e27853ba.jpg"
+            },
+            {
+                type: 6,
+                count: 1,
+                text: "三等奖",
+                title: "小米智能空气炸锅",
+                img: "https://img11.360buyimg.com/n1/jfs/t1/141098/23/29942/51629/6342bf76E12fd5adc/1ab47e320095e831.jpg"
+            },
+            {
+                type: 7,
+                count: 3,
+                text: "三等奖",
+                title: "小米手环7",
+                img: "https://img11.360buyimg.com/n1/s450x450_jfs/t1/196224/35/31980/32360/63ad42f8Fbf5da20f/81f5104ee5ed0239.jpg"
+            },
+            {
+                type: 8,
+                count: 1,
+                text: "三等奖",
+                title: "飞利浦电动牙刷",
+                img: "https://img12.360buyimg.com/n1/jfs/t1/187830/16/31652/60507/63c64accF8c495f84/0b93e54ef48adb27.jpg"
+            },
+            {
+                type: 9,
+                count: 2,
+                text: "三等奖",
+                title: "水月雨无线耳机",
+                img: "https://img10.360buyimg.com/n1/jfs/t1/223618/5/12009/86669/6295f8a3Ec170d584/de6154f2c98b580d.jpg"
+            }
+
+          ]
+    },
+    leftUsers:[
+        ['','刘秀兰','兔年大吉'], 
+        ['','王子冬','兔年大吉'], 
+        ['','宋文硕','兔年大吉'], 
+        ['','宋丹','兔年大吉'], 
+        ['','王岩','兔年大吉'], 
+        ['','宋玉玲','兔年大吉'],
+        ['','宋福生','兔年大吉'], 
+        ['','宋福军','兔年大吉'], 
+        ['','王振磊','兔年大吉'],
+        ['','陈改云','兔年大吉'], 
+        ['','韦小琴','兔年大吉'], 
+        ['','张润昊','兔年大吉'], 
+        ['','张芮涵','兔年大吉'], 
+        ['','张诗雨','兔年大吉'], 
+    ],
+    luckyData:{}
+}
+
+export const users = [
+    ['','刘秀兰','兔年大吉'], 
+    ['','王子冬','兔年大吉'], 
+    ['','宋文硕','兔年大吉'], 
+    ['','宋丹','兔年大吉'], 
+    ['','王岩','兔年大吉'], 
+    ['','宋玉玲','兔年大吉'],
+    ['','宋福生','兔年大吉'], 
+    ['','宋福军','兔年大吉'], 
+    ['','王振磊','兔年大吉'],
+    ['','陈改云','兔年大吉'], 
+    ['','韦小琴','兔年大吉'], 
+    ['','张润昊','兔年大吉'], 
+    ['','张芮涵','兔年大吉'], 
+    ['','张诗雨','兔年大吉'], 
+]
